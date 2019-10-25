0ea06617db9ad9674690cf208c3184ba78f89e8e
Yanzhi Ding <cs120fa19ab@ieng6-248.ucsd.edu>
2019-10-24 20:11:54 -0700

    problem4 test

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -127,9 +127,21 @@ public class Condition2 {
 			
 		}
 	}
+	private static void sleepForTest1(){
+		Lock lock =new Lock();
+		Condition2 cv = new Condition2(lock);
+		lock.acquire();
+		long t0 = Machine.timer().getTime();
+		System.out.println(KThread.currentThread().getName()+" sleeping");
+		cv.sleepFor(2000);
+		long t1 = Machine.timer().getTime();
+		System.out.println(KThread.currentThread().getName()+" woke up, slept for " +(t1-t0) + "ticks");
+		lock.release();
+	}
 
 	public static void selfTest(){
 		new InterlockTest();
+		sleepForTest1();
 	}
 
 
